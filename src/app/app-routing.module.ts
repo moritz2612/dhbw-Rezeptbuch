b5180457cceb1f5c4e47c3f780b96db1b41f3311
Moritz <moritz.bier@gmail.com>
2019-07-20 23:16:21 +0200

    added routing for recipe components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { RecipeOverviewComponent } from './recipe/recipe-overview/recipe-overview.component';
+import { RecipeAddComponent } from './recipe/recipe-add/recipe-add.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'recipes', component: RecipeOverviewComponent },
+  { path: 'recipe-add', component: RecipeAddComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,20 @@
+import { RecipeOverviewComponent } from './recipe/recipe-overview/recipe-overview.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RecipeAddComponent } from './recipe/recipe-add/recipe-add.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RecipeAddComponent,
+    RecipeOverviewComponent
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     AppRoutingModule
   ],
   providers: [],

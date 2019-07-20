f2c7bdcc19135158e49d24bb80b441a5404396e3
Moritz <moritz.bier@gmail.com>
2019-07-21 00:32:07 +0200

    added angular material and navigation fix

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { AppMaterialModule } from './app-material.module';
 import { RecipeOverviewComponent } from './recipe/recipe-overview/recipe-overview.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
@@ -5,17 +6,22 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { RecipeAddComponent } from './recipe/recipe-add/recipe-add.component';
 import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavbarComponent } from './recipe/navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    NavbarComponent,
     RecipeAddComponent,
     RecipeOverviewComponent
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AppMaterialModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]

f884d91be9d2d477de74b4e0428b4a0aa68e409d
Moritz <moritz.bier@gmail.com>
2019-07-23 23:30:09 +0200

    added firebase config

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyCfHDQqWjf7KaLPhutuqzs56rporJsI9tY',
+    authDomain: 'rezeptbuch-80e65.firebaseapp.com',
+    databaseURL: 'https://rezeptbuch-80e65.firebaseio.com',
+    projectId: 'rezeptbuch-80e65',
+    storageBucket: 'gs://rezeptbuch-80e65.appspot.com',
+    messagingSenderId: '866271807595',
+    appId: '1:866271807595:web:1e4168cfc7737d8c'
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyCfHDQqWjf7KaLPhutuqzs56rporJsI9tY',
+    authDomain: 'rezeptbuch-80e65.firebaseapp.com',
+    databaseURL: 'https://rezeptbuch-80e65.firebaseio.com',
+    projectId: 'rezeptbuch-80e65',
+    storageBucket: 'gs://rezeptbuch-80e65.appspot.com',
+    messagingSenderId: '866271807595',
+    appId: '1:866271807595:web:1e4168cfc7737d8c'
+  }
 };
 
 /*

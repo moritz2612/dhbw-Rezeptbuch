a59315654e14aa87ce4bcd6aab7458e9f9d1109b
Moritz <moritz.bier@gmail.com>
2019-08-03 23:39:51 +0200

    modified edit and overview component, to work properly

--- src/app/recipe/recipe-edit/recipe-edit.component.ts
+++ src/app/recipe/recipe-edit/recipe-edit.component.ts
@@ -1,4 +1,13 @@
+import { finalize } from 'rxjs/operators';
+import { AngularFireStorage } from '@angular/fire/storage';
+import { IngredientDialogComponent } from './../../shared/components/ingredient-dialog/ingredient-dialog.component';
+import { MatSnackBar, MatDialog } from '@angular/material';
+import { Location } from '@angular/common';
+import { RecipeService } from './../../shared/services/recipe.service';
+import { IRecipe } from './../../shared/models/IRecipe';
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-recipe-edit',
@@ -6,10 +15,69 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-edit.component.scss']
 })
 export class RecipeEditComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+  recipePlaceHolderImageUrl: string;
+  recipe: IRecipe;
+  isUploading = false;
+  // tslint:disable-next-line
+  constructor(private dialog: MatDialog, private storage: AngularFireStorage, private snackBar: MatSnackBar, private location: Location, private route: ActivatedRoute, private recipeService: RecipeService, private router: Router) {
+    this.recipePlaceHolderImageUrl = environment.recipePlaceHolderImageUrl;
   }
 
+  ngOnInit() {
+    // get id from url through observable
+    this.route.params.subscribe(params => {
+      const id: string = params['id'];
+      const all = this.recipeService.getAll();
+      this.recipe = this.recipeService.getById(all, id);
+    });
+  }
+
+  uploadFile(event) {
+    this.isUploading = true;
+    const file = event.target.files[0];
+    const id = Math.random().toString(36).substring(2);
+    const ref = this.storage.ref(id);
+    const task = ref.put(file);
+
+    console.log(task);
+    task.snapshotChanges().pipe(
+      finalize(() => {
+        ref.getDownloadURL().subscribe(url => {
+          console.log(url);
+          this.recipe.ImageUrl = url;
+          this.isUploading = false;
+        });
+      })).subscribe();
+  }
+
+  removeIngredient(index) {
+    // remove ingredient at given index
+    this.recipe.Ingredients.splice(index, 1);
+  }
+
+  openDialog(): void {
+    const dialogRef = this.dialog.open(IngredientDialogComponent, {
+      width: '250px',
+      data: {}
+    });
+
+    dialogRef.afterClosed()
+      .subscribe(result => { // subscribe to the response
+        if (result) {
+          // push item into ingredients array
+          this.recipe.Ingredients.push(result);
+        }
+      });
+  }
+
+  cancel() {
+    // navigate back
+    this.router.navigate(['/recipes']);
+  }
+
+  update() {
+    this.recipeService.update(this.recipe);
+    this.snackBar.open('Recipe', 'successfully updated', { duration: 2500 });
+    this.router.navigate(['/recipes']);
+  }
 }
--- src/app/recipe/recipe-overview/recipe-overview.component.ts
+++ src/app/recipe/recipe-overview/recipe-overview.component.ts
@@ -1,6 +1,9 @@
+import { Location } from '@angular/common';
+import { MatSnackBar } from '@angular/material';
 import { RecipeService } from './../../shared/services/recipe.service';
 import { IRecipe } from './../../shared/models/IRecipe';
 import { Component, OnInit } from '@angular/core';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-recipe-overview',
@@ -8,13 +11,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-overview.component.scss']
 })
 export class RecipeOverviewComponent implements OnInit {
-  private items: IRecipe[];
-  constructor(private recipeService: RecipeService) {
-
+  items: IRecipe[];
+  recipePlaceHolderImageUrl;
+  constructor(private snackBar: MatSnackBar, private location: Location, private recipeService: RecipeService) {
+    this.recipePlaceHolderImageUrl = environment.recipePlaceHolderImageUrl;
   }
 
   ngOnInit() {
     this.items = this.recipeService.getAll();
   }
 
+  delete(recipe: IRecipe) {
+    this.items = this.recipeService.delete(recipe);
+    this.snackBar.open(recipe.Name, 'successfully deleted', { duration: 2500 });
+  }
 }

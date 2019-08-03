102243e6aeeefceac8d398fcade3a9bd2a4610d3
Moritz <moritz.bier@gmail.com>
2019-08-03 21:13:12 +0200

    updated interface, added id, added Helper class to generate guid

--- src/app/shared/models/IRecipe.ts
+++ src/app/shared/models/IRecipe.ts
@@ -2,10 +2,11 @@ import { IGroceryList } from './IGroceryList';
 import { IIngredient } from './IIngredient';
 
 export interface IRecipe {
-    Name: string;
-    ImageUrl?: string;
-    Created: Date;
-    LastEdited: Date;
-    Ingredients: IIngredient[];
-    Description: string;
+  Id: string;
+  Name: string;
+  ImageUrl?: string;
+  Created: Date;
+  LastEdited: Date;
+  Ingredients: IIngredient[];
+  Description: string;
 }

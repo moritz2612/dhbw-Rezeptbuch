b6b87341a459875de178b31b20a6c1ca97c80be2
Moritz <moritz.bier@gmail.com>
2019-08-03 23:36:01 +0200

    recipe details

--- src/app/recipe/recipe-details/recipe-details.component.ts
+++ src/app/recipe/recipe-details/recipe-details.component.ts
@@ -1,4 +1,8 @@
+import { IRecipe } from './../../shared/models/IRecipe';
+import { ActivatedRoute } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
+import { RecipeService } from 'src/app/shared/services/recipe.service';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-recipe-details',
@@ -6,10 +10,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-details.component.scss']
 })
 export class RecipeDetailsComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+  recipePlaceHolderImageUrl: string;
+  recipe: IRecipe;
+  constructor(private route: ActivatedRoute, private recipeService: RecipeService) {
+    this.recipePlaceHolderImageUrl = environment.recipePlaceHolderImageUrl;
   }
 
+  ngOnInit() {
+    // subscribe to params observable
+    this.route.params.subscribe(params => {
+      const recipeIdParam = 'id';
+      const id: string = params[recipeIdParam];
+      const allRecipes: IRecipe[] = this.recipeService.getAll();
+      // get recipe
+      this.recipe = this.recipeService.getById(allRecipes, id);
+    });
+  }
 }
